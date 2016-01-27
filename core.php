0b6ca10561f9476f030128ed982023197420a7a9
Gonzalo Novo <luis.gonzalo.novo@gmail.com>
2016-01-26 23:28:13 -0300

    Se empezo a crear el core de las urls amigables

--- core.php
+++ core.php
@@ -15,11 +15,47 @@ if( !isset($_SESSION['LANGUAGE_CODE']) OR !in_array($_SESSION['LANGUAGE_CODE'], 
 }else{
     $language_code = $SESSION['LANGUAGE_CODE'];
 }
+switch($language_code){
+    case 'EN':
+        //
+        break;
+    case 'ES':
+    default:
+        //
+}
 
 //*******************************************************
 //*	INICIO DEL SITIO
 require	dirname(__FILE__) . '/includes/loader.php';
 //*******************************************************
+
+//*******************************************************
+//*	CHEQUEO DE LAACCIÓN A REALIZAR DEPENDIENDO DE LA URI
+$accion_del_sitio   = null;
+$request_uri        = trim($_SERVER['REQUEST_URI'], '/');
+
+if( !$request_uri ){
+    $accion_del_sitio = 'es_home';
+}else{
+
+    $request_uri = explode('/', $request_uri);
+
+    if( $request_uri[0] == 'procesos' ){
+        // Si entra por acá no habría que levantar plantillas con smarty
+        //
+    }else{
+        // Salida estandar por smarty
+        if( strpos($request_uri[0], 'formulario-de') !== false ){
+            //
+        }elseif( strpos($request_uri[0], 'formulario-de') !== false ){
+            //
+        }
+    }
+
+}
+
+//*******************************************************
+
 
 //*******************************************************
 // Muestro el template
